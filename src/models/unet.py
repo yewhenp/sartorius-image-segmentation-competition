3bf50999a2f4255a832e9112b75b95e6b5c030fe
Pavlo Hilei <p.hilei@ucu.edu.ua>
2022-01-15 23:54:07 +0200

    Update unet

--- src/models/unet.py
+++ src/models/unet.py
@@ -35,20 +35,20 @@ def get_unet(input_data_shape, output_channels=1) -> keras.Model:
     inputs = keras.Input(input_data_shape)
 
     # TODO: regularization
-    s1, p1 = encoder_block(inputs, 16)
-    # s1, p1 = encoder_block(inputs, 64)
-    # s2, p2 = encoder_block(p1, 128)
-    # s3, p3 = encoder_block(p2, 256)
-    # s4, p4 = encoder_block(p3, 512)
+    # s1, p1 = encoder_block(inputs, 16)
+    s1, p1 = encoder_block(inputs, 64)
+    s2, p2 = encoder_block(p1, 128)
+    s3, p3 = encoder_block(p2, 256)
+    s4, p4 = encoder_block(p3, 512)
     #
-    # b1 = conv_block(p4, 1024)
-    b1 = conv_block(p1, 32)
+    b1 = conv_block(p4, 1024)
+    # b1 = conv_block(p1, 32)
     #
-    # d1 = decoder_block(b1, s4, 512)
-    # d2 = decoder_block(d1, s3, 256)
-    # d3 = decoder_block(d2, s2, 128)
-    # d4 = decoder_block(d3, s1, 16)
-    d4 = decoder_block(b1, s1, 16)
+    d1 = decoder_block(b1, s4, 512)
+    d2 = decoder_block(d1, s3, 256)
+    d3 = decoder_block(d2, s2, 128)
+    d4 = decoder_block(d3, s1, 64)
+    # d4 = decoder_block(b1, s1, 16)
 
     outputs = layers.Conv2D(output_channels, 1, padding="same", activation="sigmoid")(d4)
 
